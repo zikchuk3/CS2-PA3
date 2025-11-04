172ed5388410b61b43686f0214da10770fad5e9b
Zikora C <zchukwumah17@gmail.com>
2025-11-04 09:51:47 -0500

    11/4/25 class time
    need to find a way to delete a certain place in the bookmarks

--- directory.py
+++ directory.py
@@ -1,11 +1,11 @@
 '''
 A directory of food pantrys/banks/non profits (focus is in the DMV)
 '''
+import os #to delete a file
 import json #for json file
 # json files are formatted in an easier way then dictionaries 
 with open("food_banks.json", "r") as file:
     info = json.load(file)
-
 
 # for error handling
 def error():
@@ -17,7 +17,7 @@ def error():
 
 # directory funcs for D M and V when user chooses from menu
 def dc_directory():
-    city = "Washington DC"
+    city = "washington dc"
     places = info[city]
 
     for i, place in enumerate(places, start = 1):
@@ -88,6 +88,7 @@ def all_directory(city):
 def learn(city, index):
     place = info[city][index]
 
+    #printing things in the json file
     print()
     print(f"---- {place['name']} ----")
     print(f"Address: {place["address"]}")
@@ -109,23 +110,43 @@ def learn(city, index):
         error()
         user_input = input("Would you like to bookmark this place? (y)es or (n)o: ")
 
-
 #if the user wants to add to bookmark place
 def add_list(bookmarked_place):
     # first they name their list (file)
     name_input = input("What is your name so I can add this to your bookmarks?: ")
-    user_file = name_input+".txt" #.txt by default
+    # to check if that file name already exists so it doesnt add to bookmarks.txt again
+    name_check = name_input+".txt"
+    x = open("bookmarks.txt", "r")
+    check = x.readlines()
+    if name_check+"\n" in check:
+        print("Added bookmark to your existing bookmark list.")
+        with open(name_check, "a") as f:
+            f.write(f" \n Name: {bookmarked_place['name']} \n")
+            f.write(f"Address: {bookmarked_place['address']} \n")
+            f.write(f"Hours: {bookmarked_place['hours']} \n")
+            f.write(f"Phone: {bookmarked_place['phone']} \n")
+            f.write(f"Summary: {bookmarked_place['summary']} \n")
+            f.write(f"Website: {bookmarked_place['website']} \n")
+    # if file doesnt exist already
+    else:
+        print("Creating a new bookmark list...")
+        user_file = name_input+".txt" #.txt by default
+        with open(user_file, "a") as f:
+            f.write(f" \n Name: {bookmarked_place['name']} \n")
+            f.write(f"Address: {bookmarked_place['address']} \n")
+            f.write(f"Hours: {bookmarked_place['hours']} \n")
+            f.write(f"Phone: {bookmarked_place['phone']} \n")
+            f.write(f"Summary: {bookmarked_place['summary']} \n")
+            f.write(f"Website: {bookmarked_place['website']} \n")
 
-    with open(user_file, "w") as f:
-        f.write(f"{bookmarked_place}")
-    with open("bookmarks.txt", "a") as g:
-        g.write(user_file + "\n")
-
-    print("Okay! Your bookmark has been saved! View your bookmarks throught the menu")
+        with open("bookmarks.txt", "a") as g:
+            g.write(user_file + "\n")
+            print("Okay! Your bookmark has been saved! View your bookmarks throught the menu")
 
 # to show bookmarks
 def show_list():
     try:
+        # read the bookmarks list if theres anything in it
         with open("bookmarks.txt", "r") as g:
             bookmarks = [line.strip() for line in g.readlines() if line.strip()]
     except FileNotFoundError:
@@ -140,7 +161,7 @@ def show_list():
     for i, bookmark in enumerate(bookmarks, 1):
         print(f"{i}. {bookmark}")
     
-    choice = input("Enter the number of your bookmark list so you can see your bookmarks or 'b' to go back.")
+    choice = input("Enter the number of your bookmark list so you can see your bookmarks or 'b' to go back: ")
     if choice.lower() == "b":
         return
     try:
@@ -148,25 +169,68 @@ def show_list():
         1 <= choice <= len(bookmarks)
         chosen_bookmark = bookmarks[choice -1]
         with open(chosen_bookmark, "r") as f:
-            f.readlines()
+            inside = f.readlines()
+            # join is to put the lines back in a string using a space as a separater 
+            print("\n" + "".join(inside) + "\n")
+
+            save_choices = ["yes", "y"]
+            back_choices = ["n", "no"]
+            delete_choice = input("Is there any bookmark you would like to delete? (y)es or (n)o: ")
+            if delete_choice in save_choices:
+                del_place = input("Which bookmark would you like to delete? (type the name of the place): ")
+                if del_place in inside:
+                    delete_list(chosen_bookmark, del_place)
+            elif delete_choice in back_choices:
+                return
+            else:
+                error()
+                delete_choice = input("Is there any bookmark you would like to delete? (y)es or (n)o: ")
+
     except ValueError:
         print("Please enter a number shown.")
 
 
-#what the user can input in main
+
+# to clear bookmark in bookmark list (trying to make it so it is a certain bookmark instead of whole list)
+def delete_list(bookmark_list, bookmark_place):
+    
+    place = bookmark_place
+
+    # open the bookmark list
+    with open(f"{bookmark_list}", "r") as g:
+        # check if the place the person inputed is in the list
+        bookmarks = [line.strip() for line in g.readlines() if f"{place}" in line]
+    if not bookmarks:
+        print("There is no bookmark with that place in this list.")
+        return
+    else:
+        print()
+    
+    #print(f"Succesfully deleted {place} from your list")
+
+
+# MENU
+# used DICTIONARY here
 def menu():
+    menu = {
+        "1": "Washington DC",
+        "2": "Browse cities in Maryland",
+        "3": "Browse cities in Virginia",
+        "4": "Type in your city",
+        "5": "Show bookmarks",
+        "6": "Exit"
+    }
+
     print("---- What will you choose? ----")
-    print("1. Washington DC")
-    print("2. Browse cities in Maryland")
-    print("3. Browse cities in Virginia")
-    print("4. Enter your city")
-    print("5. Exit")
+    for key, value in menu.items():
+        print(f"{key}. {value}")
+
 
 #main function
 def main():
     # error handling lists
-    choices = ["1", "2", "3", "4", "Washington DC", "DC", "Maryland", "MD", "Virginia", "VA"]
-    stop_choices = ["5", "Exit"]
+    choices = ["1", "2", "3", "4", "5", "Washington DC", "DC", "Maryland", "MD", "Virginia", "VA","city", "show bookmarks", "bookmarks", "show"]
+    stop_choices = ["6", "Exit"]
 
 
     # intro
@@ -187,15 +251,17 @@ def main():
             md_directory()
         elif choice == "3" or choice == "Virginia" or choice == "VA":
             va_directory()
-        elif choice == "4":
-            city = input("Enter your city: ")
+        elif choice == "4" or choice == "city":
+            city = input("Type in your city: ").lower()
             all_directory(city)
+        elif choice == "5" or choice == "show bookmarks" or choice == "show" or choice == "bookmarks":
+            show_list()
         else:
             error()
         print()
         menu()
         choice = input()
 
-
+    print("Goodbye! ")
 
 main()
